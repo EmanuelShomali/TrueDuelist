1ff4fe4dad5f33da1b09f7ec5ad17cf0bc0c923f
Emanuel Shomali <emanuelmichael@hotmail.com>
2022-11-18 17:06:08 -0500

    Commit 014
    Clusterfuck attempt at adding authentication

---
+++ aws_react/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "trueduelist2035557a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
